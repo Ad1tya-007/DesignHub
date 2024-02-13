1ae7d587bf63ce35b0db5daaa1caf3e61d1d933d
Ad1tya-007 <adityakul0314@gmail.com>
2024-02-13 12:29:45 -0800

    added live cursor

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+'use client';
+
+import Live from '@/components/Live';
+
 export default function Page() {
   return (
-    <div className="h-[100vh] w-full flex justify-center text-center items-center">
-      <h1 className="text-2xl text-white">LIVEBLOCKS FIGMA CLONE</h1>
+    <div>
+      <Live />
     </div>
   );
 }
