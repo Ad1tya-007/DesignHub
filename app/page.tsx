3a3ae2bfb5741018db277d5a016a4a543740a751
Ad1tya-007 <adityakul0314@gmail.com>
2024-02-17 11:27:29 -0800

    fixed free drawing feature

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,7 @@ import {
   handleCanvasObjectScaling,
   handleCanvasSelectionCreated,
   handleCanvaseMouseMove,
+  handlePathCreated,
   handleResize,
   initializeFabric,
   renderCanvas,
@@ -162,6 +163,13 @@ export default function Page() {
       handleCanvasObjectScaling({
         options,
         setElementAttributes,
+      });
+    });
+
+    canvas.on('path:created', (options: any) => {
+      handlePathCreated({
+        options,
+        syncShapeInStorage,
       });
     });
 
